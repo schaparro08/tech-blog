1a96dd24fdf89a2dc94fa3a51b60b457dd65f7a1
schaparro08 <101684009+schaparro08@users.noreply.github.com>
2022-05-09 21:24:00 -0400

    addition of home route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,18 +3,59 @@ const { Post, Comment, User } = require("../models");
 
 router.get("/", async (req, res) => {
     // get all posts for the homepage
-});
+    try {
+        const postsData = await Post.findAll({
+
+        });
+        const post = postsData.get({ plain: true });
+    
+        res.render('posts', {
+          post,
+          logged_in: req.session.logged_in
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
 
 router.get("/post/:id", async (req, res) => {
     // get a single post
+    try {
+        const postsData = await Post.findByPk(req.params.id, {
+          include: [
+            {
+              model: User,
+              attributes: ['username'],
+            },
+          ],
+        });
+    
+        const project = projectData.get({ plain: true });
+    
+        res.render('project', {
+          ...project,
+          logged_in: req.session.logged_in
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 router.get("/login", (req, res) => {
     // login
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('login');
 });
 
 router.get("/signup", (req, res) => {
     // signup
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    } res.render('signup');
 })
 
 module.exports = router;
